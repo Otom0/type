d399313bd2ee2b944add9e35bf9da94a6eddbdbe
Robert Dolgopolov <siidousdart@gmail.com>
2021-03-01 23:49:11 +0500

    type

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("byte min " + Byte.MIN_VALUE +  " byte max "+ Byte.MAX_VALUE);
+        System.out.println("short min " + Short.MIN_VALUE +  " short max "+ Short.MAX_VALUE);
+        System.out.println("int min " + Integer.MIN_VALUE +  " int max "+ Integer.MAX_VALUE);
+        System.out.println("long min " + Long.MIN_VALUE +  " int max "+ Long.MAX_VALUE);
+        System.out.println("float min " + Float.MIN_VALUE +  " float max "+ Float.MAX_VALUE);
+        System.out.println("double min " + Double.MIN_VALUE +  " double max "+ Double.MAX_VALUE);
+
+    }
+}
